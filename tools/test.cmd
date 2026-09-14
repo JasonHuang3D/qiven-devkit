15f5ae098ab83307e885b7875a9fe100c749cd5a
JasonHuang <jasonhuang1988@gmail.com>
2026-09-14 14:30:00 +0800

    test(devkit): include CMD control-flow regression

--- tools/test.cmd
+++ tools/test.cmd
@@ -2,5 +2,7 @@
 setlocal
 call "%~dp0resolve-toolchain.cmd"
 if errorlevel 1 exit /b 1
+"%QIVEN_CMAKE%" -DDEVKIT_ROOT="%~dp0.." -P "%~dp0cmd-control-flow-test.cmake"
+if errorlevel 1 exit /b 1
 "%QIVEN_CMAKE%" -DDEVKIT_ROOT="%~dp0.." -DQIVEN_TOOLCHAIN_ROOT_TEST="%QIVEN_TOOLCHAIN_ROOT%" -P "%~dp0test.cmake"
 exit /b %errorlevel%
