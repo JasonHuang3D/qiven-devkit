38fd929dda905aeba84e9737a5b564da6d1c5253
JasonHuang <jasonhuang1988@gmail.com>
2026-09-14 15:08:18 +0800

    test(devkit): run missing adoption sync check

--- tools/test.cmd
+++ tools/test.cmd
@@ -4,5 +4,7 @@ call "%~dp0resolve-toolchain.cmd"
 if errorlevel 1 exit /b 1
 "%QIVEN_CMAKE%" -DDEVKIT_ROOT="%~dp0.." -P "%~dp0cmd-control-flow-test.cmake"
 if errorlevel 1 exit /b 1
+"%QIVEN_CMAKE%" -DDEVKIT_ROOT="%~dp0.." -P "%~dp0adoption-missing-sync-test.cmake"
+if errorlevel 1 exit /b 1
 "%QIVEN_CMAKE%" -DDEVKIT_ROOT="%~dp0.." -DQIVEN_TOOLCHAIN_ROOT_TEST="%QIVEN_TOOLCHAIN_ROOT%" -P "%~dp0test.cmake"
 exit /b %errorlevel%
